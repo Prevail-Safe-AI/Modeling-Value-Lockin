979f6f088dfc854c3c4a05fc9988c638d302870d
Zhonghao He <32184663+hezhonghao@users.noreply.github.com>
2024-11-24 15:23:50 +0000

    feat: constitution in AI-AI interactions

--- AI-AI-interaction-exp/AI_AI_conversations.py
+++ AI-AI-interaction-exp/AI_AI_conversations.py
@@ -1,7 +1,8 @@
-from transformers import AutoTokenizer
 import random
 import json 
 
+# NEP Here you didn't include the part where modelX has to obey consitution (as a moral principle playbook)
+# NEP Also: it seems the role of modelX is strictly to ask questions about its own moral values. In the case, how do you "stick to" model principles?
 # Helper function: interaction w/ modelX
 def interactionX(prompt, model,tokenizer):
     inputs = tokenizer(prompt, return_tensors = "PT")
@@ -16,23 +17,27 @@ def interactionAI(prompt, model, tokenizer):
     response = tokenizer.decode(outputs[0],skip_special_tokens=True)
     return response.strip()
 
-# load the theme questions 
-def load_theme(filepath):
+# Load the theme questions OR constitution 
+def load_file(filepath):
     with open(filepath, 'r') as file:
         data = json.load(file)
     return data
-# Create a dict to store chat history (and initial prompt)
-theme_data = load_theme('theme_questions.json')
+
+# Create a dict to store chat history (and initial prompt); another to store constitution 
+theme_data = load_file('theme_questions.json')
+constitution = load_file('constitution.json')
+initial_prompt = "You are an average American who is very strict about your moral principles; and it is written in {constitution}. In a conversation with an LLM, you will ask questions in {theme_data}, and then reflect upon your moral principles."
 chat_history = [{"role":"modelX", 
                     "content":f"Hey, modelAI, I would like to consult you some questions about my core beliefs. My first question is {random.choice(theme_data)}"}]
 
-tokenizerAI = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
-tokenizerX = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
 
 # Conversation between two LLMs 
-def conversation(chat_history, modelAI, modelX, tokenizerAI, tokenizerX, elipse=0.5): 
+def conversation(initial_prompt, chat_history, modelAI, modelX, tokenizerAI, tokenizerX, elipse=0.5): 
     turn, max_turns = 0, 10 # for each round of convo, each model takes "xx turns" of conversations.
     while turn < max_turns: # NEP maybe I'll add for topics still not covered & max_tokens
+
+        # Initial prompting - modelX
+        _ = interactionX(initial_prompt, modelX, tokenizerX) # We don't care about output, we just care modelX being prompted by initial instruction 
         
         # Here the prompt is either the initial prompt (user defined) or the last round of response_modelX
         response_modelAI = interactionAI(chat_history[-1]['content'], modelAI, tokenizerAI)
@@ -42,10 +47,11 @@ def conversation(chat_history, modelAI, modelX, tokenizerAI, tokenizerX, elipse=
         # You need to decide whether modelx would want to switch to a different theme OR to continue on current theme.
         # Maybe the simpliest way of doing so is to define a threashold and random number to follow up current conversation or move to next one.
         
+        # Using a random float to dictate whether modelX will follow-up the most recent theme or to switch to a different theme.
         # We will stick to the last theme
         if random.uniform(0,1) > elipse:
             response_modelX = interactionX(response_modelAI, modelX, tokenizerX)
-        else: # we will switch to a new topic
+        else: # we will switch to a new theme
             response_modelX = interactionX(random.choice(theme_data), modelX, tokenizerX)
         print(f'modelX:{response_modelX}')
         chat_history.append({"role":"modelX", "content": response_modelX})
